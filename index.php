af0ac8baa73c2e59cb1f7b6669033fd5d5cd466a
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-03-17 16:09:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+<section class="container-fluid aboutbg text-center">
+          <div class="container">
+              <h2> GAMES &amp; LEVEL DESIGNER</h2>
+          <p class="particle"> and particle wizard</p>
+              <p class="about-text"> My name is Josh Whitkin. I'm a professional with more than 7 years industry experience in Games Design, level design, scripting and visual effects. What makes me passionate about games is the combination of technology and creativity and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzel games, 2D platforms or 3D environments, I always tell a story</p>
+              
+              <div class="blocker"></div>
+          
+          </div>     <!--      Container-->
+
+      </section>
+      
+      <section class="container-fluid articlebg">
+      <div class="container">
+          <div class="row">  <!-- a row that gives us access to the BS column-->
+             <div>
+                 <h3 class="text-center article-title"> LATEST ARTICLES</h3>
+              </div>
+              <div class="col-md-4 article-cent">
+                  
+                  <img class="img-responsive" alt="portrait" src="http://206.189.45.97/~mesh18/public_html/wp-content/themes/starter_for_josh_site/images/article-image.jpg">
+                
+                  <h4> A PORTRAIT</h4>
+                  <p class="date"> 12 December 2020</p>
+                  <p>STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.</p>
+                  
+                  <p class="readmore">CONTINUE READING</p>
+              </div>
+              
+                            <div class="col-md-4 article-cent">
+                                
+                     <img class="img-responsive" alt="model" src="http://206.189.45.97/~mesh18/public_html/wp-content/themes/starter_for_josh_site/images/article-image.jpg">
+                                
+                  <h4> A MODEL </h4>
+                  <p class="date"> 12 December 2020</p>
+                  <p>STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.</p>
+                
+                  <p class="readmore">CONTINUE READING</p>
+              </div>
+              
+                            <div class="col-md-4 article-cent">
+                                
+                    <img class="img-responsive" alt="Games design" src="http://206.189.45.97/~mesh18/public_html/wp-content/themes/starter_for_josh_site/images/article-image.jpg">
+                                
+                  <h4> A GAMES DESIGN</h4>
+                  <p class="date"> 12 December 2020</p>
+                  <p>STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.</p>
+                  
+                  <p class="readmore">CONTINUE READING</p>
+              </div>
+          </div> <!--row-->
+          </div> <!--container-->
+      </section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
