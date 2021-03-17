1853cf253cddcd6be7525089aff2616aceccb1c2
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-03-17 16:09:18 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="container-fluid"> <!--div for the main image to stretch to the edge of the page-->
+                  <div class="row"><!-- div to hold the content in the middle of the page-->
+                      <div class="col-md-6 text-center footer-content"><!-- a row that gives us access to the BS column-->
+                          <p> <?php bloginfo('name'); ?></p>
+                           <img src="http://206.189.45.97/~mesh18/public_html/wp-content/themes/starter_for_josh_site/images/tumblr.jpg" alt="tumblr">
+                          <img src="http://206.189.45.97/~mesh18/public_html/wp-content/themes/starter_for_josh_site/images/ rss.jpg" alt="rss">
+                      </div>
+                      <div class="col-md-6 text-center footer-content">
+                      <p> Terms and Conditions
+                          </p>
+                         <img src="http://206.189.45.97/~mesh18/public_html/wp-content/themes/starter_for_josh_site/images/ linked-in.jpg" alt="linked-in">
+                          <img src="http://206.189.45.97/~mesh18/public_html/wp-content/themes/starter_for_josh_site/images/ facebook.jpg" alt="facebook">
+                      </div> <!--row -->
+                  </div> <!--container-->
+              </footer>
+          
+
+      
+      
+  </body>
+
+</html>
+<?php wp_footer(); ?>
