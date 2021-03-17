6ca3135cbb0267589be40963027e6a49d4cd4fa0
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-03-17 16:28:07 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
                   <div class="row"><!-- div to hold the content in the middle of the page-->
                       <div class="col-md-6 text-center footer-content"><!-- a row that gives us access to the BS column-->
                           <p> <?php bloginfo('name'); ?></p>
-                           <img src="http://206.189.45.97/~mesh18/wp-content/themes/starter_for_josh_site/images/Tumblr.jpg" alt="tumblr">
+                           <img src="http://206.189.45.97/~mesh18/wp-content/themes/starter_for_josh_site/images/tumblr.jpg" alt="tumblr">
                           <img src="http://206.189.45.97/~mesh18/wp-content/themes/starter_for_josh_site/images/rss.jpg" alt="rss">
                       </div>
                       <div class="col-md-6 text-center footer-content">
