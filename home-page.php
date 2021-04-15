ae9ab51ecb58b9adbd15b2e30ebd7d3faf582777
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-04-15 22:34:13 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Home page
+*/
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+<section class="container-fluid aboutbg text-center">
+          <div class="container">
+                           
+              <div class="blocker"></div>
+          
+          </div>     <!--      Container-->
+
+      </section>
+      
+      <section class="container-fluid articlebg">
+      <div class="container">
+          <div class="row">  <!-- a row that gives us access to the BS column-->
+             <div>
+                 <h3 class="text-center article-title wow bounveInUp"> LATEST ARTICLES</h3>
+              </div>
+              <div class="col-md-4 article-cent">                   
+              </div>
+              
+                            <div class="col-md-4 article-cent">   
+              </div>
+              
+                            <div class="col-md-4 article-cent">
+              </div>
+          </div> <!--row-->
+          </div> <!--container-->
+      </section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
